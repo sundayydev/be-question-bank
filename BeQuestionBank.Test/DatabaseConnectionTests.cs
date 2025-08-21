8b7792526fbedccd2312cf5e9f92076e0b9e94b7
SundayyDev <sundayy.dev@gmail.com>
2025-08-20 20:40:58 -0700

    Add DatabaseConnectionTests for AppDbContext

---
+++ BeQuestionBank.Test/DatabaseConnectionTests.cs
@@ -0,0 +1,30 @@
+﻿using BeQuestionBank.Core.Configurations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace BeQuestionBank.Test;
+
+public class DatabaseConnectionTests
+{
+    [Fact]
+    public async Task CanConnectToDatabase()
+    {
+        // Load configuration
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false)
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("PostgresConnection");
+
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseNpgsql(connectionString)
+            .Options;
+
+        using var context = new AppDbContext(options);
+        var canConnect = await context.Database.CanConnectAsync();
+
+        Assert.True(canConnect);
+    }
+}
