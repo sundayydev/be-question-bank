03cefc0e90ba98e9293b1dc7cd2964cc3b3a88d1
Lê Duy Hoàng <duyhoanggl98@gmail.com>
2025-10-08 09:01:31 +0700

    fix codede

--- FEQuestionBank.Client/Services/ApiResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FEQuestionBank.Client.Services;
-
-public class ApiResponse<T>
-{
-    public int StatusCode { get; set; }
-    public string Message { get; set; } = string.Empty;
-    public T? Data { get; set; }
-}
