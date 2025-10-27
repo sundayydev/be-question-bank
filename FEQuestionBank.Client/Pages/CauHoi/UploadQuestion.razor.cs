6df057a43938b6000a2779150a2b6082d7fb0464
Lê Duy Hoàng <129831728+Elihoang@users.noreply.github.com>
2025-10-27 21:53:21 +0700

    update fe with upload question, single ,add new infocard

---
+++ FEQuestionBank.Client/Pages/CauHoi/UploadQuestion.razor.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FEQuestionBank.Client.Pages.CauHoi;
+
+public partial class UploadQuestion : ComponentBase
+{
+}
---
+++ FEQuestionBank.Client/Pages/NguoiDung/NguoiDung.razor.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FEQuestionBank.Client.Pages.NguoiDung;
+
+public partial class NguoiDung : ComponentBase
+{
+}
